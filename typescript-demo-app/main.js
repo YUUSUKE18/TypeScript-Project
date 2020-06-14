35602308f0ae6dc864b8eefed1493524d52700a2
Yuusuke Okamoto <yuusuke.o.work.1018@gmail.com>
2020-06-14 16:48:33 +0900

    Build Typescript Env using yarn

---
+++ typescript-demo-app/main.js
@@ -0,0 +1,8 @@
+"use strict";
+var character = "taro";
+console.log(character);
+var inputs = document.querySelectorAll("input");
+console.log(inputs);
+inputs.forEach(function (input) {
+    console.log(input);
+});
